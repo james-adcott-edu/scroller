de67635572ed9b7bf10901151326510bfb565139
James Adcott <l19rvzv3@students.codeinstitute.net>
2025-01-23 19:28:53 +0000

    add static css

--- config/settings.py
+++ config/settings.py
@@ -137,6 +137,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Ensure STATICFILES_DIRS is defined to include your static directory if it's not within an app
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
